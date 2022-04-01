92b44fe3ab934fd0702fd52642721a0dabe1c1e5
Rachel Nascimento <rachelle.nascimento@gmail.com>
2022-04-01 16:33:35 +0200

    edit a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
