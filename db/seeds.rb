841c720fb00a04f5e727c1ecd9888b1da420a452
Rachel Nascimento <rachelle.nascimento@gmail.com>
2022-04-01 15:33:58 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database......"
+
+Task.destroy_all
+
+puts "everything destroyed!"
+puts "Creating tasks ..."
+
+Task.create!(title: "Lessive", details: "Faire une machine de couleurs", completed: false)
+Task.create!(title: "Ménage", details: "Faire la poussière", completed: false)
+Task.create!(title: "Code", details: "Faire une Todo app", completed: false)
+Task.create!(title: "Courses", details: "Acheter de l'huile d'olive", completed: false)
+
+puts 'Finished!'
